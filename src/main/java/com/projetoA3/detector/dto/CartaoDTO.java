8a63417d01de560e6da82b5810b769fc610bc188
Airam <viniairam2@gmail.com>
2025-09-23 12:07:34 -0300

    Create CartaoDTO.java

---
+++ src/main/java/com/projetoA3/detector/dto/CartaoDTO.java
@@ -0,0 +1,41 @@
+package com.projetoA3.detector.dto;
+public class CartaoDTO {
+
+    private String numero;
+    private String validade;
+    private String nomeTitular;
+    private Long usuarioId; 
+
+  
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+}
