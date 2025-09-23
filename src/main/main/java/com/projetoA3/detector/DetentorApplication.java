b98e0369b9c1bfd34e834bc74cf6e6481f70babf
Airam <viniairam2@gmail.com>
2025-09-23 12:03:56 -0300

    Create DetentorApplication.java

---
+++ src/main/main/java/com/projetoA3/detector/DetentorApplication.java
@@ -0,0 +1,13 @@
+package com.projetoA3.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetentorApplication.class, args);
+	}
+
+}
