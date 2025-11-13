18eb52498fc20409c2f7d4755d3f5b007294f092
Eduardo Travassos <169100607+EduTNV@users.noreply.github.com>
2025-11-13 18:52:51 -0300

    Refactor CartaoRepositorio to simplify email query

--- src/main/java/com/projetoA3/detector/repository/CartaoRepositorio.java
+++ src/main/java/com/projetoA3/detector/repository/CartaoRepositorio.java
@@ -2,8 +2,6 @@ package com.projetoA3.detector.repository;
 
 import com.projetoA3.detector.entity.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query; // Importar Query
-import org.springframework.data.repository.query.Param; // Importar Param
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +9,6 @@ import java.util.List;
 @Repository
 public interface CartaoRepositorio extends JpaRepository<Cartao, Long> {
 
-    List<Cartao> findByUsuarioId(Long usuarioId);
-
-    // --- MÉTODO ATUALIZADO ---
-    // Tornamos a consulta explícita com @Query para garantir que funciona
-    @Query("SELECT c FROM Cartao c WHERE c.usuario.email = :email")
-    List<Cartao> findByUsuarioEmail(@Param("email") String email);
+    // "Busque uma lista de Cartoes pelo Id do seu atributo 'usuario'"
+    List<Cartao> findByUsuarioEmail(String email);
 }
